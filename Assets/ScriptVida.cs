3742938eac7c8a87f4fb13647cdbbf0ada6826f3
VictorGasperi <victorgasperi@gmail.com>
2022-04-12 22:52:03 -0300

    Arrumando o script da vida

--- Assets/ScriptVida.cs
+++ Assets/ScriptVida.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 public class ScriptVida : MonoBehaviour
 {
-    public float VidaDaTropa;
-    public float dano;
-    public float getVidaDaTropa(){
-        return (this.VidaDaTropa);
-    }
-    public float aplicarDano(float dano){
-        float novaVida = getVidaDaTropa() - dano;
-        this.VidaDaTropa = novaVida;
-        return (VidaDaTropa);
+    [SerializeField] private float vidaDaTropa;
+    [SerializeField] private float dano;
+    public float GetVidaDaTropa() => vidaDaTropa;
+    
+    public float AplicarDano(float dano){
+        float novaVida = GetVidaDaTropa() - dano; 
+        vidaDaTropa = novaVida; 
+        return (vidaDaTropa); 
     }
     
     void Start()
     {   
-        Debug.Log(getVidaDaTropa());
-        Debug.Log(aplicarDano(dano));
-    }
-
-    
-    void Update()
-    {
-        
+        Debug.Log(GetVidaDaTropa());
+        Debug.Log(AplicarDano(dano));
     }
 }
