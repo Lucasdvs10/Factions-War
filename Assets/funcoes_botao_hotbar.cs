7a1f4ab94f26d4e58705eb08fe632f81049c58e9
Dragonrhs <ryuske2009@hotmail.com>
2022-04-18 20:23:59 -0300

    hotbar inicial

---
+++ Assets/funcoes_botao_hotbar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class funcoes_botao_hotbar : MonoBehaviour
+{
+  
+
+public void Funcao_botao(){
+    Debug.Log("Ai Ai");
+}
+}
