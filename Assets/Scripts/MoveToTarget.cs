96640ed7d5bbaffbbcd8beab0f41a5446f99560d
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-04-23 21:22:41 -0300

    Deixei mais legível e encapsulei as variáveis correntamente

--- Assets/Scripts/MoveToTarget.cs
+++ Assets/Scripts/MoveToTarget.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 public class MoveToTarget : MonoBehaviour
 {
-    public Transform target;
-    public float speed;
+    [SerializeField]
+    private Transform _target;
+    
+    [SerializeField]
+    private float _speed;
     
     void Update()
     {
         //Distance between Attacker and Target
-        Vector3 distanceFromTarget = target.position - transform.position;
+        Vector3 distanceFromTarget = _target.position - transform.position;
+        
         //Normalized gets direction with a given vector
         Vector3 direction = distanceFromTarget.normalized;
-        Vector3 velocity = direction * speed;
+        Vector3 velocity = direction * _speed;
         
         //Changes position of Attacker through time
         transform.Translate(velocity * Time.deltaTime);
