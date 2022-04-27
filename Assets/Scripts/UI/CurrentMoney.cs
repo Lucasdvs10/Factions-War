6f539fc2e7a7bb93283fc2461944fb30dea560f5
HoshizoraAmatsu <guishikaw@gmail.com>
2022-04-27 18:27:42 -0300

    Passive Income script
    Script to increase the in-game money at a set interval.
    
    variables:
    
    CurrentMoney currentMoney -> Set the script CurrentMoney. TODO: Get the class automatically.
    
    float startTime -> Starts calling the function after this set time.
    
    float secondsBetweenRepeat -> Calls the function at the pace set by this.
    
    int passiveValue -> Value to add to the money total.
    
    Functions:
    
    void Start() -> Calls AddPassiveIncome() function after startTime seconds and every time secondsBetweenRepeat seconds passes.
    
    void AddPassiveIncome() -> Adds passiveValue to game money.

--- Assets/Scripts/UI/CurrentMoney.cs
+++ Assets/Scripts/UI/CurrentMoney.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class CurrentMoney : MonoBehaviour
 {
     [SerializeField] private int _currentMoney;
---
+++ Assets/Scripts/UI/PassiveIncome.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveIncome : MonoBehaviour
+{
+    [SerializeField] CurrentMoney currentMoney;
+    [SerializeField] float startTime = 2.0f;
+    [SerializeField] float secondsBetweenRepeat = 2.0f;
+    [SerializeField] int passiveValue = 0;
+
+    //Calls AddPassiveIncome() function after startTime seconds and every time secondsBetweenRepeat seconds passes.
+    void Start()
+    {
+        InvokeRepeating("AddPassiveIncome", startTime, secondsBetweenRepeat);
+    }
+
+    //Adds passiveValue to game money.
+    void AddPassiveIncome()
+    {
+        currentMoney.UpdateMoney(passiveValue);
+    }
+}
