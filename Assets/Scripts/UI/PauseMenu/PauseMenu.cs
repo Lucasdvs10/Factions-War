e881e846bfbcfe9eac125268a00fcd6a58bc20af
Tagly <leotagliati56@gmail.com>
2022-05-22 12:45:12 -0300

    Feat: Menu pause UI

---
+++ Assets/Scripts/UI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+
+    // Resume is called once the game is unpaused
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    // Pause is called once the game is paused
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting");
+    }
+
+    // Start is called once the game starts
+    void Start()
+    {
+        Resume();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If escape is pressed, pause or resume the game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+
+    }
+}
