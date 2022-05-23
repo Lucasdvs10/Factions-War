e339a2253b98c02d492528ce39be2e0d3fa6d4a0
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-05-23 14:43:50 -0300

    Rotacao das tropas funciona agr

--- Assets/Scripts/Animations/SniperRotate.cs
+++ Assets/Scripts/Animations/SniperRotate.cs
@@ -1,16 +1,15 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SniperRotate : MonoBehaviour
 {   
     private TargetRadar _currentTargetChanged;
     private Transform _currentTargetTransform;
-    private Vector3 _vectorDirection;
-    private Quaternion _rotation;
 
 
     void Start()
     {
-        _currentTargetChanged = GetComponent<TargetRadar>();
+        _currentTargetChanged = GetComponentInChildren<TargetRadar>();
         _currentTargetChanged.CurrentTargetChangedEvent += RotateSniper;
 
     }
@@ -19,9 +18,15 @@ public class SniperRotate : MonoBehaviour
     {
         if(_currentTarget != null){
             _currentTargetTransform = _currentTarget.GetComponent<Transform>();
-        _vectorDirection = (_currentTargetTransform.position - transform.position).normalized;
-        _rotation = Quaternion.LookRotation(_vectorDirection);
-        transform.rotation = _rotation;
-        }   
+            var vectorDirection = (_currentTargetTransform.position - transform.position).normalized;
+            var rotation = Mathf.Atan2(vectorDirection.y, vectorDirection.x);
+    
+            transform.rotation = quaternion.Euler(Vector3.forward * rotation);
+        
+        }
+
+        else {
+            transform.rotation = Quaternion.identity;
+        }
     }
 }
