84928f12c7544d70aa5b7f8ee4f49d280e6e0b3e
VictorGasperi <victorgasperi@gmail.com>
2022-05-24 22:04:46 -0300

    Arrumando os arquivos

--- Assets/Scripts/Animations/Sniper/Rotation.cs
+++ Assets/Scripts/Animations/Sniper/Rotation.cs
@@ -1,32 +1,34 @@
+using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class SniperRotate : MonoBehaviour
 {   
+    
     private TargetRadar _currentTargetChanged;
     private Transform _currentTargetTransform;
-
 
     void Start()
     {
         _currentTargetChanged = GetComponentInChildren<TargetRadar>();
         _currentTargetChanged.CurrentTargetChangedEvent += RotateSniper;
-
     }
 
     void RotateSniper(GameObject _currentTarget)
     {
-        if(_currentTarget != null){
+        if (_currentTarget != null)
+        {
             _currentTargetTransform = _currentTarget.GetComponent<Transform>();
             var vectorDirection = (_currentTargetTransform.position - transform.position).normalized;
             var rotation = Mathf.Atan2(vectorDirection.y, vectorDirection.x);
-    
             transform.rotation = quaternion.Euler(Vector3.forward * rotation);
-        
         }
-
         else {
             transform.rotation = Quaternion.identity;
         }
     }
+
+
+
+
 }
