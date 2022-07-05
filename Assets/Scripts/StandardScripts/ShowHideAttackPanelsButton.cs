002a555b42f84f63da80c1663c9b4ecf41379949
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-07-05 10:41:52 -0300

    Refatorei o script de desativar gameobjs

--- Assets/Scripts/StandardScripts/ShowHideAttackPanelsButton.cs
+++ Assets/Scripts/StandardScripts/ShowHideAttackPanelsButton.cs
@@ -2,36 +2,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShowHideAttackPanelsButton : MonoBehaviour
-{
+public class ShowHideAttackPanelsButton : MonoBehaviour{
+    [SerializeField] private List<GameObject> _gameObjectsList;
+    private bool _activated;
 
-    [SerializeField] private GameObject northPanel;
-    [SerializeField] private GameObject southPanel;
-    [SerializeField] private GameObject eastPanel;
-    [SerializeField] private GameObject westPanel;
+    public void ShowHideAttackPanels() {
+        Text t = transform.Find("Text").GetComponent<Text>();
+        
+        if (_gameObjectsList.Count <= 0) return; //Guardian Claw
 
-    public void ShowHideAttackPanels()
-    {
-        if (northPanel != null && southPanel != null && eastPanel != null && westPanel != null)
-        {
-            if (northPanel.activeSelf && southPanel.activeSelf && eastPanel.activeSelf && westPanel.activeSelf)
-            {
-                northPanel.SetActive(false);
-                southPanel.SetActive(false);
-                eastPanel.SetActive(false);
-                westPanel.SetActive(false);
-                Text t = transform.Find("Text").GetComponent<Text>();
-                t.text = "Exibir painéis de ataque";
-            }
-            else
-            {
-                northPanel.SetActive(true);
-                southPanel.SetActive(true);
-                eastPanel.SetActive(true);
-                westPanel.SetActive(true);
-                Text t = transform.Find("Text").GetComponent<Text>();
-                t.text = "Esconder painéis de ataque";
-            }
+        
+        if (!_activated) {
+            t.text = "Exibir painéis de ataque";
+            _activated = true;
+        }
+
+        else {
+            t.text = "Esconder painéis de ataque";
+            _activated = false;
+        }
+
+
+        foreach (var gameObj in _gameObjectsList) {
+            gameObj.SetActive(!_activated);
         }
     }
 }
+
+
+
+
+
+
+
