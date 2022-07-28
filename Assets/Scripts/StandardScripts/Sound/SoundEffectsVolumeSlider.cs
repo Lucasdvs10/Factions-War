a343c2d517915cb38815ec8ec03bd3d96a6c603e
enric0-s <enricoricsaez@gmail.com>
2022-07-28 19:17:24 -0300

    Adicionei sliders de volume e implementei som quando nn pode spawnar defensor

---
+++ Assets/Scripts/StandardScripts/Sound/SoundEffectsVolumeSlider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundEffectsVolumeSlider : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _audioMixer;
+
+    public void SetVolume(float sliderValue)
+    {
+        _audioMixer.SetFloat("SoundEffectsVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
