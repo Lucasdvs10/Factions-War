1c36d1e70e84a7727e43903c169d85b1f49b5123
HoshizoraAmatsu <guishikaw@gmail.com>
2022-08-14 16:20:04 -0300

    Som quando torre leva dano
    Modificação no script TowerHealth.cs onde quando houver instância de dano, toca um som.

--- Assets/Scripts/StandardScripts/TowerHealth.cs
+++ Assets/Scripts/StandardScripts/TowerHealth.cs
@@ -8,20 +8,27 @@ public class TowerHealth : MonoBehaviour
     public HealthBar healthBar;
     public float maxHealth;
     [SerializeField] private float _towerHealth;
+
+    public AudioClip _audioClip;
     
     public static event Action<float> OnTowerDamageTriggered;
     public static event Action OnZeroTowerHealth;
     public float GetTowerHealth() => _towerHealth;
+
+    private AudioSource _audioSource;
     
     public void Start(){
         healthBar.SetMaxHealth(maxHealth);
         healthBar.SetHealth(_towerHealth);
+
+        _audioSource = GetComponent<AudioSource>();
     }
     
     public float ApplyDamage(float damage) {
         OnTowerDamageTriggered?.Invoke(damage);
         _towerHealth -= damage;
         healthBar.SetHealth(_towerHealth);
+        _audioSource.PlayOneShot(_audioClip, 1.0F);
         if (_towerHealth <= 0)
         {
             OnZeroTowerHealth?.Invoke();
